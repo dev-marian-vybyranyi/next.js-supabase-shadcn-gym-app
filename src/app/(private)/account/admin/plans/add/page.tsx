89ae94fa499427852a7a7e6ea129458ef52b8e66
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-21 11:40:24 +0200

    add admin pages for plan management including list, add, and edit functionalities, along with a new `PageTitle` UI component.

---
+++ src/app/(private)/account/admin/plans/add/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/components/ui/page-title";
+
+const AddPlanePage = () => {
+  return (
+    <div>
+      <PageTitle title="Add Plan" />
+    </div>
+  );
+};
+
+export default AddPlanePage;
---
+++ src/app/(private)/account/admin/plans/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/components/ui/page-title";
+
+const EditPlanePage = () => {
+  return (
+    <div>
+      <PageTitle title="Edit Plan" />
+    </div>
+  );
+};
+
+export default EditPlanePage;
