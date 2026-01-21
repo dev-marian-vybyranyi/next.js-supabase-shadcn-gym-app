5c91d4cb5cf0cd89fc0dff96c749eee44a3934f0
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-21 08:58:07 +0200

    Integrate Clerk middleware and display user information on the account page.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,10 +1,21 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-const AccountPage = () => {
+const AccountPage = async () => {
+  const result = await currentUser();
+  console.log(result);
+
+  const name = result?.firstName + " " + result?.lastName;
+  const clerkUserId = result?.id;
+  const email = result?.emailAddresses[0].emailAddress;
+
   return (
     <div className="p-5">
       <h1>AccountPage</h1>
       <UserButton fallback="/" afterSignOutUrl="/" />
+      <h1>{name}</h1>
+      <h1>{clerkUserId}</h1>
+      <h1>{email}</h1>
     </div>
   );
 };
