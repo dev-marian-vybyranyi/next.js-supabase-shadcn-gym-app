7b45e518024ca485a4308fc4cbaf5ffb764193a4
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-21 11:07:55 +0200

    Implement loading and error states in `PrivateLayout` with a new `Spinner` component.

--- src/custom-layout/private-layout.tsx
+++ src/custom-layout/private-layout.tsx
@@ -3,26 +3,41 @@ import { IUser } from "@/interfaces";
 import { ReactNode, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import Header from "./header";
+import Spinner from "@/components/ui/spinner";
 
 const PrivateLayout = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<IUser | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   const fetchUser = async () => {
     try {
+      setLoading(true);
       const response: any = await getCurrentUserFromSupabase();
       if (!response.success) {
         throw new Error(response.error);
       } else {
         setUser(response.data);
       }
-    } catch (error) {
+    } catch (error: any) {
+      setError(error.message);
       toast.error("An error occurred while fetching user data");
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchUser();
   }, []);
+
+  if (loading) {
+    return <Spinner parentHeight="100vh" />;
+  }
+
+  if (error) {
+    return <div className="p-5">{error}</div>;
+  }
 
   return (
     <div>
