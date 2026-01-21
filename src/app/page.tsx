dcd47a92eda126e350f4d7edef418065445e1efd
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-21 08:20:06 +0200

    Implement initial landing page UI with a header, hero section, and custom styling, integrating `lucide-react` for icons and updating `package-lock.json`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { ArrowDownToLine } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="p-5 flex flex-col gap-5">
-      <h1>Fit Gym</h1>
-      <Button className="w-max">Button</Button>
+    <div className="home-parent py-10 px-20">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-white uppercase">Fit.Gym</h1>
+        <Button variant={"outline"}>Sign-in</Button>
+      </div>
+      <div className="flex flex-col justify-center items-center h-[80vh] gap-7 mt-20">
+        <h1 className="text-6xl font-bold text-center">
+          <b className="text-white">Fit.</b>
+          <b className="text-green-600">Gym</b>
+        </h1>
+        <p className="text-sm font-semibold text-gray-500">
+          A perfect gym for you to get fit and healthy with the best trainersand
+          equipment.
+        </p>
+
+        <Button variant={"outline"}>Explore Plans</Button>
+
+        <ArrowDownToLine
+          size={20}
+          color="gray"
+          className="animate-bounce cursor-pointer mt-20"
+        />
+      </div>
     </div>
   );
 }
